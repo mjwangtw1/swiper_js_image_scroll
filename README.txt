293b0618975bcf2effa933e96c1da23cb34994d6
mjwangtw1 <mjwangtw@gmail.com>
2014-04-15 17:01:12 +0800

    first_time trying
    1. trying to use swiper
    2. created a readme to explain.

---
+++ README.txt
@@ -0,0 +1,2 @@
+This is just a simple practice with Git.
+Also play with Swiper a little bit.
---
+++ src/try.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    $(function() {
+
+    //     var mySwiper = $('.swiper-container').swiper({
+ 			// pagination: '.pagination',
+    //         mode: 'horizontal',
+    //         loop: true,
+    //         //grabCursor: true,
+    //         paginationClickable: true
+
+    //     });
+
+
+        var mySwiper = new Swiper('.swiper-container', {
+            pagination: '.pagination',
+            loop: true,
+            grabCursor: true,
+            paginationClickable: true,
+            mode: 'horizontal'
+        });
+
+
+
+
+
+
+
+
+
+    });
+
+
+}); //end of doc_ready;
